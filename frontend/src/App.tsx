52762c0f84d8e83ff4b1273806635c782a0a9a40
Uljas Wennstrom <uljas.wennstrom@gmail.com>
2023-08-21 22:54:36 +0200

    -

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import Canvas from "./features/canvas/Canvas";
+import { fabric } from "fabric"; // Import the fabric object from the Fabric.js library
 
-function App() {
+export default function App() {
+  const editorRef = useRef<any>(null); // To store the editor instance
+
+  const handleEditorReady = (editor : any) => {
+    editorRef.current = editor; // Store the editor instance for manipulation
+    
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FabricJS React Sample</h1>
+      <Canvas onReady={handleEditorReady} />
     </div>
   );
 }
-
-export default App;
---
+++ frontend/src/features/canvas/Canvas.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
+
+interface CanvasProps {
+  onReady: Function
+}
+
+function Canvas({ onReady }: CanvasProps) {
+  const { editor } = useFabricJSEditor();
+  
+  useEffect(() => {
+    onReady(editor);
+  }, [editor, onReady]);
+
+  return (
+    <div className="canvas-component">
+      {/* Rendering the canvas */}
+      <FabricJSCanvas className="sample-canvas" />
+    </div>
+  );
+}
+
+export default Canvas;
