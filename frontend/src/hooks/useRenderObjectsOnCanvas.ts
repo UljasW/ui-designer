db194cc82615c5f1bbb9b4259b4bc4843afe0b84
Uljas Wennstrom <uljas.wennstrom@gmail.com>
2023-10-04 00:00:38 +0200

    WOOORKING

--- frontend/src/hooks/useRenderObjectsOnCanvas.ts
+++ frontend/src/hooks/useRenderObjectsOnCanvas.ts
@@ -3,10 +3,38 @@ import { fabric } from "fabric";
 export default function useRenderObjectsOnCanvas() {
   const renderObjectsOnCanvas = (
     canvasInstance: fabric.Canvas,
-    objects: any
+    objectsContainer: any
   ) => {
-    console.log("Rendering objects:", objects);
-    // Logic for rendering objects on the canvas
+    console.log("Original objects:", objectsContainer);
+
+    const sortedDataObjects = objectsContainer.objects
+      .map((obj: any) => JSON.parse(obj.data))
+      .sort((a: any, b: any) => a.layerIndex - b.layerIndex);
+
+    console.log("Sorted objects:", sortedDataObjects);
+
+    sortedDataObjects.forEach((obj: any) => {
+      let fabricObj;
+
+      switch(obj.type) {
+        case 'rect':
+          fabricObj = new fabric.Rect(obj);
+          break;
+        case 'text':
+          fabricObj = new fabric.Text(obj.text, obj);
+          break;
+        // ... add cases for other object types as needed
+        default:
+          console.error("Unsupported object type: ", obj.type);
+          return;
+      }
+
+      // Add the fabric object to the canvas
+      canvasInstance.add(fabricObj);
+    });
+
+    canvasInstance.renderAll();
+
     return;
   };
 
