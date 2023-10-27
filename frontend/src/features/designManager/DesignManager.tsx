72fe797152b00c11e78d48669025d83aa5007478
21ulwe <21ulwe@stockholmscience.se>
2023-10-27 12:35:31 +0200

    is owner

--- frontend/src/features/designManager/DesignManager.tsx
+++ frontend/src/features/designManager/DesignManager.tsx
@@ -14,7 +14,9 @@ export default function DesignManager() {
     createDesign(designName, localStorage.getItem("jwt") || "");
   };
 
-  const handleDesignNameChange = (event: ChangeEvent<HTMLInputElement>): void => {
+  const handleDesignNameChange = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
     setDesignName(event.target.value);
   };
 
@@ -66,12 +68,12 @@ export default function DesignManager() {
       </form>
       <div>
         {designList?.map((design) => (
-          <div key={design.id} style={{ marginBottom: "15px"}}>
+          <div key={design.id} style={{ marginBottom: "15px" }}>
             <h3 style={{ color: "#333" }}>{design.name}</h3>
             <h4>Is owner: {design.isOwner.toString()}</h4>
-            
+
             <p style={{ color: "#666" }}>{design.id}</p>
-            <div>
+            <div style={{display:"flex", flexDirection:"row"}}>
               <button
                 onClick={(e: any) => {
                   handleDesignStart(e, design.id);
@@ -88,40 +90,45 @@ export default function DesignManager() {
               >
                 View
               </button>
-              <button
-                onClick={(e: any) => {
-                  e.preventDefault();
-                  handleDesignInvite(e, design.id)
-                }}
-                style={{
-                  padding: "10px",
-                  fontSize: "16px",
-                  color: "#fff",
-                  backgroundColor: "blue",
-                  border: "none",
-                  cursor: "pointer",
-                  marginRight: "10px",
-
-                }}
-              >
-                Invite user
-              </button>
-              <button
-                onClick={(e: any) => {
-                  e.preventDefault();
-                  handleDesignDelete(design.id);
-                }}
-                style={{
-                  padding: "10px",
-                  fontSize: "16px",
-                  color: "#fff",
-                  backgroundColor: "#f44336",
-                  border: "none",
-                  cursor: "pointer",
-                }}
-              >
-                Delete
-              </button>
+              {design.isOwner ? (
+                <div>
+                  <button
+                    onClick={(e: any) => {
+                      e.preventDefault();
+                      handleDesignInvite(e, design.id);
+                    }}
+                    style={{
+                      padding: "10px",
+                      fontSize: "16px",
+                      color: "#fff",
+                      backgroundColor: "blue",
+                      border: "none",
+                      cursor: "pointer",
+                      marginRight: "10px",
+                    }}
+                  >
+                    Invite user
+                  </button>
+                  <button
+                    onClick={(e: any) => {
+                      e.preventDefault();
+                      handleDesignDelete(design.id);
+                    }}
+                    style={{
+                      padding: "10px",
+                      fontSize: "16px",
+                      color: "#fff",
+                      backgroundColor: "#f44336",
+                      border: "none",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Delete
+                  </button>{" "}
+                </div>
+              ) : (
+                <div></div>
+              )}
             </div>
           </div>
         ))}
