1c07c84071182955a521533ce8256b7ed05e7c83
Uljas Wennstrom <uljas.wennstrom@gmail.com>
2024-01-08 08:13:54 +0100

    -

--- backend/server.ts
+++ backend/server.ts
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 const io = new Server(server, {
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,8 +1,8 @@
 const url = "localhost:3001"
 
 const ip = "192.168.68.59:3001"
-const apiUrl = "http://" + ip;
-const socketUrl = "ws://" + ip;
+const apiUrl = "http://" + url;
+const socketUrl = "ws://" + url;
 
 export {apiUrl, socketUrl};
 
